9d64f484772cf71084d185b078fe8699980a911d
Xuejianchuan <252837474@qq.com>
2026-08-10 14:57:56 +0800

    chore: remove leftover pdfjs helper modules after history rewrite

--- mediaTypes/pdf/PdfJs.ts
+++
@@ -1,4 +0,0 @@
-/**only for external use, internal cannot use, may cause circular reference */
-import * as pdfjsLib from 'pdfjs-dist/legacy/build/pdf.mjs';
-export default pdfjsLib
-
--- mediaTypes/pdf/PdfJsViewer.ts
+++
@@ -1,6 +0,0 @@
-/**only for external use, internal cannot use, may cause circular reference */
-import * as pdfjsViewer from 'pdfjs-dist/legacy/web/pdf_viewer.mjs';
-
-
-export default pdfjsViewer
-
